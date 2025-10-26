3af42061be063665e46c66c341eb558656e08d39
Muhammad Reyhan <reyhn829@gmail.com>
2025-10-26 10:25:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      {/* <h1 className="text-4xl text-red-600 font-bold">Hello World</h1> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+const Navbar = () => {
+  return (
+    <div className="navbar py-7 flex items-center justify-between">
+      <div className="logo">
+        <h1 className="text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white">Portofolio</h1>
+      </div>
+      <ul className="menu flex items-center gap-10 md:static fixed left-1/2 -translate-x-1/2 md:translate-x-0 -top-10 opacity-0">
+        <li>
+          <a href="#" className="text-lg font-medium">
+            Beranda
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-lg font-medium">
+            Tentang
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-lg font-medium">
+            Proyek
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-lg font-medium">
+            Kontak
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
