d5bae3cdc54d741b54cec121057ebc854b72f50a
reyhanx <reyhn829@gmail.com>
2025-10-29 06:55:42 +0700

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import DataImage from './data';
+
+
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo repellat ut minus nemo facere? Ipsa, tempore, tempora voluptatum eius maxime veritatis repellat laboriosam a ad perspiciatis ipsum aperiam hic commodi neque harum! Dolorem fuga inventore tempore repellat, quibusdam cupiditate sunt harum, molestias, culpa enim iure. Incidunt architecto eaque eius ipsum velit aut, et, esse tempore quidem, iste soluta magni a nesciunt fuga amet explicabo nemo eveniet doloremque fugit tempora saepe. Quos alias quis qui minus sint tempora molestias sapiente ex, a aut, odio temporibus repellat. Animi temporibus labore eos minus neque! Laboriosam aut maxime ipsum recusandae exercitationem ratione ducimus placeat quaerat eligendi, eum dolores odit explicabo similique! Veniam, odit tempora. Vel similique rerum magni vitae eaque recusandae inventore alias commodi, blanditiis tenetur libero ex tempora nesciunt? Inventore dolorem iure at impedit temporibus quod odio ab voluptatem enim ratione, dolores eaque nisi omnis exercitationem neque modi. Nesciunt iusto fuga beatae neque quisquam ipsum atque laboriosam autem nemo, minima ad dignissimos mollitia maiores dolorem, reiciendis maxime officia sapiente ex natus adipisci repudiandae animi ipsam impedit? Doloribus, expedita quod enim nulla laudantium optio, facilis voluptatibus perspiciatis eaque quis id natus rerum voluptatem repellat maiores deserunt, voluptates odio maxime? Repudiandae sapiente nemo odio tenetur.</h1>
+      <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
+        <div>
+          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
+            <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" srcset="" />
+            <q> Ingin Menjadi Programmer Handal, Namun Enggan Ngoding</q>
+          </div>
+          <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya ir jendral wito</h1>
+          <p className='text-base/loose mb-6 opacity-50 '>
+            saya malas ngoding, tiap hari ngoding terus. disaat kodeku berwarna merah, disitulah dosenku marah</p>
+            <div className='flex items-center sm:gap-4 gap-2'>
+              <a href="#" className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600">
+                Download CV <i className="ri-download-line ri-lg"></i>
+                </a>
+              <a href="#" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600">
+                Lihat Proyek <i className="ri-arrow-down-line ri-lg"></i>
+                </a>
+            </div>
+        </div>
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" srcset="" />
+      </div>
     </>
   )
 }
