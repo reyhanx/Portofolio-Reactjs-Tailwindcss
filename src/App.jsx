a82ed5b235163b1e174cf1342b17ec39fb0a31ff
reyhanx <reyhn829@gmail.com>
2025-10-29 05:43:03 +0700

    Navbar Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      {/* <h1 className="text-4xl text-red-600 font-bold">Hello World</h1> */}
+      <h1 className="text-3xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo repellat ut minus nemo facere? Ipsa, tempore, tempora voluptatum eius maxime veritatis repellat laboriosam a ad perspiciatis ipsum aperiam hic commodi neque harum! Dolorem fuga inventore tempore repellat, quibusdam cupiditate sunt harum, molestias, culpa enim iure. Incidunt architecto eaque eius ipsum velit aut, et, esse tempore quidem, iste soluta magni a nesciunt fuga amet explicabo nemo eveniet doloremque fugit tempora saepe. Quos alias quis qui minus sint tempora molestias sapiente ex, a aut, odio temporibus repellat. Animi temporibus labore eos minus neque! Laboriosam aut maxime ipsum recusandae exercitationem ratione ducimus placeat quaerat eligendi, eum dolores odit explicabo similique! Veniam, odit tempora. Vel similique rerum magni vitae eaque recusandae inventore alias commodi, blanditiis tenetur libero ex tempora nesciunt? Inventore dolorem iure at impedit temporibus quod odio ab voluptatem enim ratione, dolores eaque nisi omnis exercitationem neque modi. Nesciunt iusto fuga beatae neque quisquam ipsum atque laboriosam autem nemo, minima ad dignissimos mollitia maiores dolorem, reiciendis maxime officia sapiente ex natus adipisci repudiandae animi ipsam impedit? Doloribus, expedita quod enim nulla laudantium optio, facilis voluptatibus perspiciatis eaque quis id natus rerum voluptatem repellat maiores deserunt, voluptates odio maxime? Repudiandae sapiente nemo odio tenetur.</h1>
     </>
   )
 }
